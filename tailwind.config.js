d4d93e8564a2b910b463ce904ecabb4a496d759f
shahin8433 <mdshahinalamsss6027@gmail.com>
2024-08-18 19:31:32 +0600

    task-3 done

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      container:{
+        center: true,
+        padding: " 1.25rem"
+      },
+      colors:{
+        primary:"#64748b"
+   
+      }
+    },
+  },
+  plugins: [],
+}
+
